aaa94279bd7d1a05f008cea0cf6f96af20087e3d
Emilie Burgun <emilie@shadamethyst.xyz>
2025-12-07 22:18:52 +0100

    :tada: Day 5

---
+++ day5.pl
@@ -0,0 +1,43 @@
+:- use_module(library(pio)).
+:- use_module(library(clpfd)).
+:- [library(dcg/basics)].
+
+parse_range(From-To) --> integer(From), "-", integer(To), "\n".
+
+parse_ranges([]) --> ("\n" ; eos).
+parse_ranges([R | Rs]) --> parse_range(R), parse_ranges(Rs).
+
+parse_ingredients([]) --> ("\n" ; eos).
+parse_ingredients([I | Is]) --> integer(I), "\n", parse_ingredients(Is).
+
+parse_problem(Rs, Is) --> parse_ranges(Rs), parse_ingredients(Is).
+read_problem(Rs, Is) :-
+    phrase_from_file(parse_problem(Rs, Is), "./inputs/day5.txt"),
+    !.
+
+is_fresh([From-To | _], I) :- I =< To, I >= From, !.
+is_fresh([_ | R], I) :- is_fresh(R, I).
+
+part1(N) :-
+    read_problem(Rs, Is),
+    part1(N, Rs, Is).
+
+part1(0, _, []).
+part1(X, Rs, [I | Is]) :-
+    part1(Y, Rs, Is),
+    (
+        is_fresh(Rs, I) -> X is Y + 1
+        ; X = Y
+    ).
+
+ranges_to_domain([From-To], From..To).
+ranges_to_domain([From-To | Rs], From..To \/ Ds) :-
+    ranges_to_domain(Rs, Ds).
+
+part2(N) :-
+    read_problem(Rs, _),
+    part2(N, Rs).
+part2(N, Rs) :-
+    ranges_to_domain(Rs, Dom),
+    X in Dom,
+    fd_size(X, N).
